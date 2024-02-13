e0a6a0260fa2cb9af02de1fe2be8ad00620293f6
Moontys <mantas.stirpeika@gmail.com>
2024-02-13 19:39:35 +0200

    directory structure & class definitions

---
+++ src/CLIApplication.php
@@ -0,0 +1,6 @@
+<?php
+// Define CLI Application class
+class CLIApplication {
+    // Implement functionality methods here
+    // View charities, add charity, edit charity, delete charity, add donation, etc.
+}
